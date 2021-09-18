4ba0e63a8eedc716d994cb9855018a061844e1e9
petch <tayanchonbc@gmail.com>
2021-09-19 00:36:46 +0700

    add auth in public content

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,14 +3,15 @@ const controller = require("../controllers/user.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //   "Access-Control-Allow-Headers",
+    //   "x-access-token, Origin, Content-Type, Accept"
+    // );
     next();
   });
 
-  app.get("/api/test/all", controller.allAccess);
+  app.get("/api/test/all",[authJwt.verifyToken], controller.allAccess);
   
   // app.get("/api/test/allmovie", controller.allMovie)
   
