0b2a2a1040d59edc338a9fcc505e81e564f60790
petch <tayanchonbc@gmail.com>
2021-09-27 00:15:39 +0700

    use local api

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  // HOST: "localhost",
-  // USER: "root",
-  // PASSWORD: "",
-  // DB: "crud_exam",
-  // dialect: "mysql",
- 
-  // pool: {
-  //   max: 5,
-  //   min: 0,
-  //   acquire: 30000,
-  //   idle: 10000
-  // }
-  HOST: "sql6.freesqldatabase.com",
-  USER: "sql6440225",
-  PASSWORD: "3U4Qa4PdWp",
-  DB: "sql6440225",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "",
+  DB: "crud_exam",
   dialect: "mysql",
+ 
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000,
-  },
+    idle: 10000
+  }
+  // HOST: "sql6.freesqldatabase.com",
+  // USER: "sql6440225",
+  // PASSWORD: "3U4Qa4PdWp",
+  // DB: "sql6440225",
+  // dialect: "mysql",
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000,
+  // },
 };
--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  // origin: "http://localhost:8081 "
-  origin: '*'
+  origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
