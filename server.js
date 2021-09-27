bc296e08778bae6fda1a3e130d952fae72c8a03c
petch <tayanchonbc@gmail.com>
2021-09-27 21:21:59 +0700

    new origin path

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  // origin: "http://localhost:8081"
+  origin : "https://reactjs-movies-crud.herokuapp.com"
 };
 
 app.use(cors(corsOptions));
